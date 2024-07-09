57ec760c136f56a05fde83a618219e41b0928418
seohyeon <uimi3606@gmail.com>
2024-07-09 20:27:07 +0900

    :sparkles: RecordPage branch 생성

--- screen/RecordPage.js
+++ screen/RecordPage.js
@@ -8,13 +8,42 @@ import {
   Modal,
   Button,
 } from "react-native";
-import Footer from "../components/Footer";
+
+import HeaderLogo from "../assets/images/headerLogo.png";
 
 export default function RecordPage({ navigation }) {
+  const handleLogoPress = () => {
+    navigation.navigate("MainPage");
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>기록페이지</Text>
-      <Footer navigation={navigation} />
-    </View>
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.container}
+    >
+      <TouchableOpacity onPress={handleLogoPress} style={styles.logoContainer}>
+        <Image source={HeaderLogo} style={styles.headerLogo} />
+      </TouchableOpacity>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0D0F35",
+  },
+  logoContainer: {
+    alignItems: "center",
+  },
+  headerLogo: {
+    width: "70%",
+    height: 100,
+  },
+});
