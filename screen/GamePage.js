c48f9dabd889c5b9493b75d0e0397d1e9bf7b15f
박찬윤 <qkcksdbs@naver.com>
2024-07-12 15:33:34 +0900

    스타일 작업완료

--- screen/GamePage.js
+++ screen/GamePage.js
@@ -1,24 +1,47 @@
-import React from "react";
-import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
-import Footer from "../components/Footer";
+import React, { useEffect } from "react";
+import { View, StyleSheet, Image, TouchableOpacity, ScrollView } from "react-native";
+import { HeaderBackButton } from "@react-navigation/elements"; // HeaderBackButton 임포트
+import SubmitButton from "../components/SubmitButton"; // SubmitButton 컴포넌트 임포트
+
+import HeaderLogo from "../assets/images/headerLogo.png"; // HeaderLogo 이미지 임포트
 
 export default function GamePage({ navigation }) {
+  const handleLogoPress = () => {
+    navigation.navigate("MainPage");
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>기억력 게임</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>TRPG 게임</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>기타 게임</Text>
-        </TouchableOpacity>
+      <View style={styles.customHeader}>
+        <HeaderBackButton
+          onPress={() => navigation.goBack()}
+          tintColor="#ffffff"
+        />
       </View>
-      <View style={styles.footer}>
-        <Footer navigation={navigation} />
-      </View>
+      <TouchableOpacity onPress={handleLogoPress} style={styles.logoContainer}>
+        <Image source={HeaderLogo} style={styles.headerLogo} />
+      </TouchableOpacity>
+      <ScrollView
+        contentContainerStyle={[
+          styles.scrollViewContent,
+          { alignItems: "center" },
+        ]}
+        style={styles.scrollView}
+      >
+        <View style={styles.buttonContainer}>
+          <SubmitButton onPress={() => {}} text="기억력 게임" />
+          <View style={{ marginVertical: 30 }} /> {/* 버튼들 사이의 간격 */}
+          <SubmitButton onPress={() => {}} text="TRPG 게임" />
+          <View style={{ marginVertical: 30 }} /> {/* 버튼들 사이의 간격 */}
+          <SubmitButton onPress={() => {}} text="기타 게임" />
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -26,30 +49,31 @@ export default function GamePage({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#0D0F35",
+  },
+  customHeader: {
+    height: 50,
+    justifyContent: "center",
+    paddingLeft: 10,
+    backgroundColor: "#0D0F35",
+    zIndex: 10,
+  },
+  logoContainer: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  headerLogo: {
+    width: "70%",
+    height: 100,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
   },
   buttonContainer: {
+    marginTop: 20,
+    alignItems: "center", // 버튼들을 수평 정렬하기 위해 추가
+  },
+  scrollView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: '#2196F3',
-    padding: 20,
-    borderRadius: 10,
-    marginVertical: 10,
-    width: 200,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-  },
-  footer: {
-    position: 'absolute',
-    bottom: 40, // Footer를 더 위로 올리기 위해 bottom 값을 40으로 설정
-    width: '100%',
   },
 });
-
