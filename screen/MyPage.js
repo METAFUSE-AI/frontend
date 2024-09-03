eee0acd51e170e0468141209a961889af78a3dae
seohyeon <uimi3606@gmail.com>
2024-09-03 12:17:57 +0900

    :sparkles: mypage 기초 화면 제작

--- screen/MyPage.js
+++ screen/MyPage.js
@@ -1,21 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   StyleSheet,
-  Text,
   Image,
   TouchableOpacity,
-  Modal,
-  Button,
+  ScrollView,
 } from "react-native";
-import axios from "axios";
-import Footer from "../components/Footer";
+import { HeaderBackButton } from "@react-navigation/elements"; // HeaderBackButton 임포트
+
+import HeaderLogo from "../assets/images/headerLogo.png"; // HeaderLogo 이미지 임포트
+
+// 마이페이지 - 테스트 결과  점수별 삼각 그래프, 테스트 결과 가져오기
 
 export default function MyPage({ navigation }) {
+  const handleLogoPress = () => {
+    navigation.navigate("MainPage");
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>마이페이지</Text>
-      <Footer navigation={navigation} />
+    <View style={styles.container}>
+      <View style={styles.customHeader}>
+        <HeaderBackButton
+          onPress={() => navigation.goBack()}
+          tintColor="#ffffff"
+        />
+      </View>
+      <TouchableOpacity onPress={handleLogoPress} style={styles.logoContainer}>
+        <Image source={HeaderLogo} style={styles.headerLogo} />
+      </TouchableOpacity>
+      <ScrollView
+        contentContainerStyle={[
+          styles.scrollViewContent,
+          { alignItems: "center" },
+        ]}
+        style={styles.scrollView}
+      ></ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0D0F35",
+  },
+  customHeader: {
+    height: 50,
+    justifyContent: "center",
+    paddingLeft: 10,
+    backgroundColor: "#0D0F35",
+    zIndex: 10,
+  },
+  logoContainer: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  headerLogo: {
+    width: "70%",
+    height: 100,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  buttonContainer: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+  scrollView: {
+    flex: 1,
+  },
+});
